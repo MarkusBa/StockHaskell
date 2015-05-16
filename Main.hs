ed4ec1848c790a99bc3de2bb3381726a405dbf13
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-05-16 10:28:16 +0200

    I don't like Haskell...

---
+++ Main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+import           Control.Monad.IO.Class  (liftIO)
+import           Control.Monad.Logger    (runStderrLoggingT)
+import           Database.Persist
+import           Database.Persist.Postgresql
+import           Database.Persist.TH
+import           Data.Time (UTCTime, getCurrentTime)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Player
+    name String
+    email String
+    deriving Show
+Item
+    symbol String
+    amount Int
+    price Double
+    idPlayer PlayerId
+    ts UTCTime default=CURRENT_TIME
+    deriving Show
+|]
+
+connStr = "host=localhost dbname=stockHaskell user=markus password=xxxx port=5432"
+
+initMe :: IO (Key Item)
+initMe = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
+  liftIO $ do
+    flip runSqlPersistMPool pool $ do
+      runMigration migrateAll
+
+      idPlayer <- insert $ Player "Test" "test@test.com"
+      time <- liftIO getCurrentTime
+      idItem1 <- insert $ Item "CASH" 10000 1.0 idPlayer time
+      insert $ Item "PAH3.DE" 0 0 idPlayer time
+      
+      
+main = initMe
