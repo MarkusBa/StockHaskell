22b50e973e5813f249fc4dc40f8c29d13e19ef45
Markus Barthlen <markus@doesntexist.com>
2015-06-29 20:02:15 +0200

    Moved database interactions to a seperate file

---
+++ DatabaseInteractions.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+module DatabaseInteractions where
+
+import           Control.Monad.IO.Class  (liftIO)
+import           Control.Monad.Logger    (runStderrLoggingT)
+import           Database.Persist
+import           Database.Persist.Postgresql
+import           Database.Persist.TH
+import           Data.Time (UTCTime, getCurrentTime)
+
+--see also https://github.com/yesodweb/yesodweb.com-content/issues/107 for issue with UTCTime
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Player
+    name String
+    email String
+    deriving Show
+Item
+    symbol String
+    amount Int
+    price Double
+    idPlayer PlayerId
+    ts UTCTime 
+    deriving Show
+|]
+
+connStr = "host=localhost dbname=stockHaskell user=markus password=1234 port=5432"
+
+initDb :: IO (Key Item)
+initDb = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
+  liftIO $ do
+    flip runSqlPersistMPool pool $ do
+      runMigration migrateAll
+
+      idPlayer <- insert $ Player "Test" "test@test.com"
+      time <- liftIO getCurrentTime
+      idItem1 <- insert $ Item "CASH" 10000 1.0 idPlayer time
+      insert $ Item "PAH3.DE" 0 0 idPlayer time
+      
+
