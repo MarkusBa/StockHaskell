a607f297ea4914829934bff414f7495c1f81aff4
Markus Barthlen <markus@doesntexist.com>
2015-06-29 22:50:25 +0200

    code compiles, result however still the same as without regexes

--- Requests.hs
+++ Requests.hs
@@ -1,11 +1,16 @@
 module Requests where
  
 import Network.HTTP
- 
+import Text.Regex.PCRE
+
 -- Perform a basic HTTP get request and return the body
 get :: String -> IO String
 get url = simpleHTTP (getRequest url) >>= getResponseBody
 
 
+getSymbolsRaw :: String -> IO String
+getSymbolsRaw sym = get $ "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=" ++ sym ++ "&callback=YAHOO.Finance.SymbolSuggest.ssCallback"
+
 getSymbols :: String -> IO String
-getSymbols sym = get $ "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=" ++ sym ++ "&callback=YAHOO.Finance.SymbolSuggest.ssCallback"
+getSymbols sym = prefix >>= \x -> return $ x =~ "YAHOO.Finance.SymbolSuggest.ssCallback\\((.*?)\\)"
+                  where prefix = getSymbolsRaw sym
