15104ac8ae425e66fdeda54e83904afd3917aa9b
Markus Barthlen <markus@doesntexist.com>
2015-06-30 19:05:34 +0200

    Haskell is amazing

--- Main.hs
+++ Main.hs
@@ -1,10 +1,10 @@
 import           DatabaseInteractions (initDb)
-import           Requests (getSymbols,getStocks)
+import           Requests (getSymbols,getStocks,getHistory)
   
 doNothing :: IO (Int)
 doNothing = return 42
 
 --main = getSymbols "yahoo"
-main = getStocks ["YHOO.MX"]
+--main = getStocks ["YHOO.MX"]
 --main = getStocks ["YHOO.MX","YHOO"]
-
+main = getHistory "BAS.DE" "0" "1" "2000" "0" "31" "2010" "w" 
--- Requests.hs
+++ Requests.hs
@@ -23,13 +23,16 @@ getSymbols sym = prefix >>= \x -> return $ justTheSecondMatch x
                         justTheSecondMatch x = extractInner x !! 0 !! 1
                         extractInner x       = x =~ "YAHOO.Finance.SymbolSuggest.ssCallback\\((.*?)\\)" :: [[String]]
 
---urlEncodeVars
 --Get the stock info for the list of symbols supplied.
---return $ getStocks ["\"YHOO.MX\""]
--- "http://query.yahooapis.com/v1/public/yql?q=select%20%2A%20from%20yahoo.finance.quotes%20where%20symbol%20in%28YHOO.MX%29&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
--- Should look like this:
--- "http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.quotes+where+symbol+in%28%22YHOO%22%29&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
-                        
+
+{-
+ The previous version with urlEncodeVars did not work properly:
+ return $ getStocks ["\"YHOO.MX\""]
+ "http://query.yahooapis.com/v1/public/yql?q=select%20%2A%20from%20yahoo.finance.quotes%20where%20symbol%20in%28YHOO.MX%29&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
+ It should look like this:
+ "http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.quotes+where+symbol+in%28%22YHOO%22%29&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
+-}
+
 getStocks :: [String] -> IO String
 getStocks symbols = get $ "http://query.yahooapis.com/v1/public/yql?" ++ getparams
                    where envparam = ("env", "http://datatables.org/alltables.env")
@@ -37,3 +40,10 @@ getStocks symbols = get $ "http://query.yahooapis.com/v1/public/yql?" ++ getpara
                          joinedSymbols = intercalate "%22,%22" symbols
                          qparam = ("q", "select+*+from+yahoo.finance.quotes+where+symbol+in%28%22" ++ joinedSymbols ++ "%22%29") 
                          getparams = "q=" ++ snd qparam ++ "&env=" ++ snd envparam ++ "&format=json"
+
+-- Yeah, I know. Too many params...
+-- getHistory "BAS.DE" "0" "1" "2000" "0" "31" "2010" "w"                         
+getHistory :: String -> String -> String -> String -> String -> String -> String -> String -> IO String
+getHistory symbol monthFrom dayFrom yearFrom monthTo dayTo yearTo stepSize = get $ "http://ichart.yahoo.com/table.csv?" ++ getparams
+                                                                             where getparams = urlEncodeVars vars
+                                                                                   vars = [("s", symbol),("a",monthFrom),("b",dayFrom),("c",yearFrom),("d",monthTo),("e",dayTo), ("f", yearTo),("g", stepSize)]
