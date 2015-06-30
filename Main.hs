9ee14d87c0f2134ba5646359d37749cd003af248
Markus Barthlen <markus@doesntexist.com>
2015-06-30 18:26:50 +0200

    Not as convenient as clojure, but it works...

--- Main.hs
+++ Main.hs
@@ -1,7 +1,14 @@
 import           DatabaseInteractions (initDb)
-import           Requests (getSymbols)
+import           Requests (getSymbols,getStocks)
   
 doNothing :: IO (Int)
 doNothing = return 42
 
-main = getSymbols "yahoo"
+--main = getSymbols "yahoo"
+main = getStocks ["YHOO.MX"]
+--main = getStocks ["YHOO.MX","YHOO"]
+
+--main = return $ getStocks ["\"YHOO.MX\""]
+-- "http://query.yahooapis.com/v1/public/yql?q=select%20%2A%20from%20yahoo.finance.quotes%20where%20symbol%20in%28YHOO.MX%29&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
+-- Should look like this:
+-- "http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.quotes+where+symbol+in%28%22YHOO%22%29&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
--- Requests.hs
+++ Requests.hs
@@ -2,6 +2,7 @@ module Requests where
  
 import Network.HTTP
 import Text.Regex.PCRE
+import Data.List (intercalate)
 
 -- Perform a basic HTTP get request and return the body
 get :: String -> IO String
@@ -15,8 +16,21 @@ getSymbolsRaw sym = get $ "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query
 
 -- This is slightly complicated because of
 -- 'No instance for (RegexContext Regex (IO String) (IO String))'
+-- The accessors !! should never fail, otherwise the application should be reevaluated as a whole.
 getSymbols :: String -> IO String
 getSymbols sym = prefix >>= \x -> return $ justTheSecondMatch x
                   where prefix               = getSymbolsRaw sym
                         justTheSecondMatch x = extractInner x !! 0 !! 1
                         extractInner x       = x =~ "YAHOO.Finance.SymbolSuggest.ssCallback\\((.*?)\\)" :: [[String]]
+
+--urlEncodeVars
+--Get the stock info for the list of symbols supplied.                        
+getStocks :: [String] -> IO String
+getStocks symbols = get $ "http://query.yahooapis.com/v1/public/yql?" ++ getparams
+                   where envparam = ("env", "http://datatables.org/alltables.env")
+                         formatparam = ("format","json")
+                         joinedSymbols = intercalate "%22,%22" symbols
+                         qparam2 = ("q", "select * from yahoo.finance.quotes where symbol in(" ++ joinedSymbols ++ ")" )
+                         qparam = ("q", "select+*+from+yahoo.finance.quotes+where+symbol+in%28%22" ++ joinedSymbols ++ "%22%29") 
+                         getparams2 = urlEncodeVars [qparam, envparam, formatparam]
+                         getparams = "q=" ++ snd qparam ++ "&env=" ++ snd envparam ++ "&format=json"
