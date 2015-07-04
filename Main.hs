d4f07a543c383cffa3af714304124a1d9b6acbab
Markus Barthlen <markus@doesntexist.com>
2015-07-04 20:21:54 +0200

    Select almost working

--- Main.hs
+++ Main.hs
@@ -1,10 +1,12 @@
-import           DatabaseInteractions (initDb)
+import           DatabaseInteractions (initDb,getItems)
 import           Requests (getSymbols,getStocks,getHistory)
-  
+import           Database.Persist.Postgresql
+
 doNothing :: IO (Int)
 doNothing = return 42
 
 --main = getSymbols "yahoo"
 --main = getStocks ["YHOO.MX"]
 --main = getStocks ["YHOO.MX","YHOO"]
-main = getHistory "BAS.DE" "0" "1" "2000" "0" "31" "2010" "w" 
+--main = getHistory "BAS.DE" "0" "1" "2000" "0" "31" "2010" "w" 
+main = getItems $ toSqlKey 1
